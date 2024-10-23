1d22ee5690fead66267b201e6680348a5870b2c4
Julia Young <young.juliamay@gmail.com>
2024-10-22 18:17:50 -0600

    final commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,23 @@
 import express from 'express';
-import router from './routes/index.js'
+import router from './routes/index.js';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.json());
+app.use(express.static('../client/dist')); 
 
+app.use(express.json());
 app.use(router);
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
